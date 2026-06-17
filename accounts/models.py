1156e36eda4dd6f8b158c956024e2f246a0e55da
notsuz <ifosuz77@gmail.com>
2026-06-17 17:55:32 +0545

    customuser Setup done

---
+++ accounts/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    phone=models.CharField(max_length=20, null=True)
+    street_address=models.CharField(max_length=200)
+    
+
+# Create your models here.
+
+class Profile(models.Model):
+    user=models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='profile')
+    profile_picture=models.ImageField(upload_to="images")
+    bio = models.TextField()
+    dob = models.DateField(null=True)
+    created_at = models.DateField(auto_now=True)
+    
+
